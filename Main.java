9a69408085a4dfb3200282c3ab64ba86a39b51ae
Your Name <you@example.com>
2025-10-27 14:05:00 +0200

    adding label

--- Main.java
+++ Main.java
@@ -1,5 +1,36 @@
+import javax.swing.*;
+
 public class Main{
     public static void main(String[] args){
-        System.out.println("test");
+        // Creating instance of JFrame
+        JFrame frame = new JFrame();
+
+        // Creating instance of JButton
+        JButton button = new JButton("Click");
+        
+        // Creating instance of JLabel
+        JLabel label = new JLabel("Hello world");
+
+        // x axis, y axis, width, height
+        button.setBounds(150, 200, 220, 50);
+
+        // label bounderies
+        label.setBounds(20,20,100,50);
+
+
+        // adding button in JFrame
+        frame.add(button);
+
+        // adding label in JFrame
+        frame.add(label);
+
+        // 400 width and 500 height
+        frame.setSize(500, 600);
+
+        // using no layout managers
+        frame.setLayout(null);
+
+        // making the frame visible
+        frame.setVisible(true);
     }
 }
